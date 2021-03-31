be30de2380fdf7d7312797b6e95a1da1ecdbc74f
Daniel Bergholz <daniel@maratonas.academy>
2021-03-31 09:27:00 -0300

    finish

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const form = document.forms.registro
-const h2 = document.querySelector('form[name=registro] h2')
-const h3 = document.querySelector('form[name=registro] h3')
-const span = document.querySelector('form[name=registro] span')
-const emailInput = document.querySelector('form[name=registro] input')
-const mainButton = document.querySelector(
-  'form[name=registro] button[type=submit]'
-)
-const discordButton = document.querySelector(
-  'form[name=registro] div.discord-button'
-)
-const a = document.querySelector('form[name=registro] div.discord-button a')
+// const form = document.forms.registro
+// const h2 = document.querySelector('form[name=registro] h2')
+// const h3 = document.querySelector('form[name=registro] h3')
+// const span = document.querySelector('form[name=registro] span')
+// const emailInput = document.querySelector('form[name=registro] input')
+// const mainButton = document.querySelector(
+//   'form[name=registro] button[type=submit]'
+// )
+// const discordButton = document.querySelector(
+//   'form[name=registro] div.discord-button'
+// )
+// const a = document.querySelector('form[name=registro] div.discord-button a')
 
-form.addEventListener('submit', handleSubmit)
+// form.addEventListener('submit', handleSubmit)
 
-function handleSubmit(e) {
-  e.preventDefault()
-  span.style.display = 'none'
-  form.style.height = '260px'
-  emailInput.style.display = 'none'
-  h2.innerHTML = 'Inscrição feita com sucesso!'
-  h3.innerHTML =
-    'Enquanto isso, você pode interagir em nossa comunidade no Discord'
-  h3.style.marginBottom = '0'
-  mainButton.style.display = 'none'
-  discordButton.style.display = 'block'
-  a.style.display = 'flex'
-}
+// function handleSubmit(e) {
+//   e.preventDefault()
+//   span.style.display = 'none'
+//   form.style.height = '260px'
+//   emailInput.style.display = 'none'
+//   h2.innerHTML = 'Inscrição feita com sucesso!'
+//   h3.innerHTML =
+//     'Enquanto isso, você pode interagir em nossa comunidade no Discord'
+//   h3.style.marginBottom = '0'
+//   mainButton.style.display = 'none'
+//   discordButton.style.display = 'block'
+//   a.style.display = 'flex'
+// }
